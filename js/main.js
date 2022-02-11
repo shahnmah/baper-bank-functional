9b08c61c61541401ab9d3eccb1486817347b4a00
shahnmah <shahn.mah@gmail.com>
2022-02-11 22:05:15 +0600

    login page done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 document.getElementById('login-btn').addEventListener('click',function(){
-    
+    const emailInput =document.getElementById('email-input').value;
+    const passwordInput =document.getElementById('password-input').value;
+    if(emailInput == 'baperbank@gmail.com' && passwordInput == '123'){
+        window.location.href = 'bank.html'
+    }
+    else{
+        alert('Wrong email or password')
+    }
 })
